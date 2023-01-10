b4c2a2d86771f900a459dbc45dca1909469622c9
BakhtawarChangezi <122357495+BakhtawarChangezi@users.noreply.github.com>
2023-01-10 15:20:45 +0500

    first commit

---
+++ q1.java
@@ -0,0 +1,30 @@
+package javalab12pro;
+import java.util.Scanner;
+
+public class q1 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[] array = getArray();
+
+		
+		System.out.print("Enter the index of the array: ");
+		try {
+			
+			System.out.println("The corresponding element value is " + array[input.nextInt()]);
+			
+		}
+		catch (ArrayIndexOutOfBoundsException ex) {
+			System.out.println("Array Out of Bound.");
+		}
+	}
+	
+	public static int[] getArray() {
+		int[] array = new int[10];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random() * 100) + 1;
+			System.out.println(array[i]);
+		}
+		return array;
+		
+	}
+}
